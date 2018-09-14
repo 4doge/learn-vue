a7d68df821bbedd07538b97e249fa2a25305ed96
Anton Boksha <antonboksha@gmail.com>
2018-09-14 13:36:40 +0300

    init repo, add lesson-02

---
+++ lesson-02/app.js
@@ -0,0 +1,51 @@
+new Vue({
+    el: '#app',
+    data: function() {
+        return {
+            showUsersTable: true,
+            users: [
+                {
+                    'first_name': 'nora',
+                    'last_name': 'rivera',
+                    'photo': 'https://randomuser.me/api/portraits/women/57.jpg'
+                },
+                {
+                    'first_name': 'lucas',
+                    'last_name': 'johnson'
+                },
+                {
+                    'first_name': 'duane',
+                    'last_name': 'meyer',
+                    'photo': 'https://randomuser.me/api/portraits/men/53.jpg'
+                },
+                {
+                    'first_name': 'ben',
+                    'last_name': 'mills',
+                    'photo': 'https://randomuser.me/api/portraits/men/69.jpg'
+                },
+                {
+                    'first_name': 'avery',
+                    'last_name': 'alexander'
+                }
+            ]
+        }
+    },
+    computed: {
+        usersLength: function() {
+            return this.users.length;
+        }
+    },
+    methods: {
+        showHideUsersTable: function() {
+            this.showUsersTable = !this.showUsersTable;
+        },
+        copied: function(value) {
+            alert(`"${ value }" copied!`);
+        }
+    },
+    filters: {
+        capitalize: function(value) {
+            return value.charAt(0).toUpperCase() + value.substr(1);
+        }
+    }
+});
